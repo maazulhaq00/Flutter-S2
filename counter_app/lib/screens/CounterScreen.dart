3785a411c9f81dcb9add593abe3fdc877a0cd791
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-05-19 09:07:13 +0500

    counter app

---
+++ counter_app/lib/screens/CounterScreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int count = 0;
+  void increment() {
+    setState(() {
+      count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter App")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Count: $count"),
+            TextButton(onPressed: increment, child: Text("Inc")),
+          ],
+        ),
+      ),
+    );
+  }
+}
